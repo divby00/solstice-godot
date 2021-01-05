ca076d91bb08a304fe9bca3f611156090be3ede4
divby00 <divisionby0x00@gmail.com>
2021-01-05 07:51:57 +0100

    Crusher fixes

--- scenes/Crusher/Crusher.gd
+++ scenes/Crusher/Crusher.gd
@@ -1,15 +1,20 @@
 extends StaticBody2D
 
-export(float) var crush_frequency = 1
+signal enemy_attacked(damage)
+
 onready var timer = $Timer
 onready var animation_player = $AnimationPlayer
 
+export(float) var crush_frequency = 1
+
 func _ready():
 	timer.wait_time = crush_frequency
+	Utils.connect_signal(self, "enemy_attacked", ResourceLoader.player, "on_enemy_attacked")
 
-func _on_Hitbox_body_entered(_body):
-	if PlayerData.status != PlayerData.Status.INVINCIBLE:
-		PlayerData.health = 0
+func _on_Hitbox_body_entered(body):
+	if body.is_in_group("PlayerGroup"):
+		if PlayerData.status != PlayerData.Status.INVINCIBLE:
+			emit_signal("enemy_attacked", 10)
 
 func _on_Timer_timeout():
 	animation_player.play("animate")
